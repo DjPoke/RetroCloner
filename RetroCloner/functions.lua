cf95a3118b850c0f8ac4429994d3f2cf878412aa
Bruno Vignoli <brunovignoli@orange.fr>
2026-04-29 05:26:17 +0200

    Add files via upload

---
+++ RetroCloner/functions.lua
@@ -0,0 +1,560 @@
+-- reset vars and arrays for the editor
+function ResetAll()
+	game_data.blocks = {}
+	game_data.blocks_data = {}
+	game_data.sprites = {}
+	game_data.animations = {}
+	game_data.animations_loop = {}
+	game_data.actors = {}
+	game_data.levels = {}
+	game_data.levels_data = {}
+	
+	img_blocks = {}
+	img_sprites = {}
+	
+	block_copy = {}
+	sprite_copy = {}
+	frame_copy = {}
+	block_data_copy = {}
+	
+	game_data.background_paper = 1
+	game_data.game_paper = 0
+	game_data.text_paper = 0
+	game_data.text_pen = 1
+	game_data.border_paper = 1
+	
+	pen_color = 1
+	selected_color = 0
+
+	maxx = 0
+	maxy = 0
+
+	current_block = 0
+	current_sprite = 0
+	current_animation = 0
+	current_frame = 0
+	current_actor = 0
+	current_entity_type = 0
+	current_player_type = 0
+	current_enemy_type = 0
+	current_bonus_type = 0
+	current_property = 0
+	current_level = 0
+	current_level_mode = 0
+	current_level_selected_block = 0
+	current_level_selected_actor = 0
+	current_level_actors_edit_mode = false
+	current_level_scroll_x = 0
+	current_level_scroll_y = 0
+	
+	block_x = 0
+	block_y = 0
+	sprite_x = 0
+	sprite_y = 0
+	
+	block_too_many_color = false
+	sprite_too_many_color = false
+end
+
+-- load a preset
+function LoadPreset(path)
+	-- remove preset data
+	preset_data = {}
+	
+	-- load preset data
+	for line in love.filesystem.lines(path) do
+		local a1 = {}
+		local a2 = {}
+		local a3 = {}
+	
+		a1 = split_string(line, ":")
+		a2 = split_string(a1[2], ",")
+		
+		table.insert(a3, a1[1])
+		
+		for i = 1, #a2 do
+			table.insert(a3, a2[i])
+		end
+		
+		table.insert(preset_data, a3)
+	end
+	
+	-- get editable palette
+	local n = PresetValue("EditablePalette")
+	
+	if n > 0 then
+		if preset_data[n][2] == "true" then
+			game_data.editable_palette = true
+		else
+			game_data.editable_palette = false
+		end
+	else
+		print("EditablePalette missing!")
+	end
+	
+	-- get max inks
+	local n = PresetValue("MaxInks")
+	
+	if n > 0 then
+		game_data.max_inks = tonumber(preset_data[n][2])
+	else
+		print("MaxInks missing!")
+	end
+
+	-- get full palette
+	local n = PresetValue("InksPalette")
+	
+	if n > 0 then
+		-- clear old full palette
+		game_data.inks_palette = {}
+		
+		-- fill new full palette
+		for i = 2, #preset_data[n] do
+			table.insert(game_data.inks_palette, preset_data[n][i])
+		end
+	else
+		print("InksPalette missing!")
+	end
+
+	-- get max palette colors
+	local n = PresetValue("MaxPens")
+	
+	if n > 0 then
+		game_data.max_pens = tonumber(preset_data[n][2])
+	else
+		print("MaxPens missing!")
+	end
+
+	-- get default palette
+	local n = PresetValue("PensPalette")
+	
+	if n > 0 then
+		-- clear old palette
+		game_data.pens_palette = {}
+		
+		-- fill new palette
+		for i = 2, #preset_data[n] do
+			table.insert(game_data.pens_palette, tonumber(preset_data[n][i]))
+		end
+	else
+		print("PensPalette missing!")
+	end
+		
+	-- get pixel doubled
+	local n = PresetValue("PixelDoubled")
+	
+	if n > 0 then
+		if preset_data[n][2] == "true" then
+			game_data.pixel_doubled = true
+			game_data.pixel_size = 2
+		else
+			game_data.pixel_doubled = false
+			game_data.pixel_size = 1
+		end
+	else
+		print("PixelDoubled missing!")
+	end
+
+	-- get colors by block
+	local n = PresetValue("ColorsByBlock")
+	
+	if n > 0 then
+		game_data.colors_by_block = tonumber(preset_data[n][2])
+	else
+		print("ColorsByBlock missing!")
+	end
+
+	-- get colors by sprite
+	local n = PresetValue("ColorsBySprite")
+	
+	if n > 0 then
+		game_data.colors_by_sprite = tonumber(preset_data[n][2])
+	else
+		print("ColorsBySprite missing!")
+	end
+
+	-- get block width
+	local n = PresetValue("BlockWidth")
+	
+	if n > 0 then
+		game_data.block_width = tonumber(preset_data[n][2])
+	else
+		print("BlockWidth missing!")
+	end
+
+	-- get block height
+	local n = PresetValue("BlockHeight")
+	
+	if n > 0 then
+		game_data.block_height = tonumber(preset_data[n][2])
+	else
+		print("BlockHeight missing!")
+	end
+
+	-- get sprite width
+	local n = PresetValue("SpriteWidth")
+	
+	if n > 0 then
+		game_data.sprite_width = tonumber(preset_data[n][2])
+	else
+		print("SpriteWidth missing!")
+	end
+
+	-- get sprite height
+	local n = PresetValue("SpriteHeight")
+	
+	if n > 0 then
+		game_data.sprite_height = tonumber(preset_data[n][2])
+	else
+		print("SpriteHeight missing!")
+	end
+
+	-- get max blocks
+	local n = PresetValue("MaxBlocks")
+	
+	if n > 0 then
+		game_data.max_blocks = tonumber(preset_data[n][2])
+	else
+		print("MaxBlocks missing!")
+	end
+
+	-- get max sprites
+	local n = PresetValue("MaxSprites")
+	
+	if n > 0 then
+		game_data.max_sprites = tonumber(preset_data[n][2])
+	else
+		print("MaxSprites missing!")
+	end
+
+	-- get max animations
+	local n = PresetValue("MaxAnimations")
+	
+	if n > 0 then
+		game_data.max_animations = tonumber(preset_data[n][2])
+	else
+		print("MaxAnimations missing!")
+	end
+
+	-- get max actors
+	local n = PresetValue("MaxActors")
+	
+	if n > 0 then
+		game_data.max_actors = tonumber(preset_data[n][2])
+	else
+		print("MaxActors missing!")
+	end
+
+	-- get max levels
+	local n = PresetValue("MaxLevels")
+	
+	if n > 0 then
+		game_data.max_levels = tonumber(preset_data[n][2])
+	else
+		print("MaxLevels missing!")
+	end
+
+	-- get screen width
+	local n = PresetValue("ScreenWidth")
+	
+	if n > 0 then
+		game_data.screen_width = tonumber(preset_data[n][2])
+	else
+		print("ScreenWidth missing!")
+	end
+
+	-- get screen height
+	local n = PresetValue("ScreenHeight")
+	
+	if n > 0 then
+		game_data.screen_height = tonumber(preset_data[n][2])
+	else
+		print("ScreenHeight missing!")
+	end
+
+	-- get border
+	local n = PresetValue("Border")
+	
+	if n > 0 then
+		if preset_data[n][2] == "true" then
+			game_data.border = true
+		else
+			game_data.border = false
+		end
+	else
+		print("Border missing!")
+	end
+
+	-- get game area x
+	local n = PresetValue("GameAreaX")
+	
+	if n > 0 then
+		table.insert(game_data.areas, {x = tonumber(preset_data[n][2])})
+	else
+		print("GameAreaX missing!")
+	end
+
+	-- get game area y
+	local n = PresetValue("GameAreaY")
+	
+	if n > 0 then
+		game_data.areas[GAME_AREA].y = tonumber(preset_data[n][2])
+	else
+		print("GameAreaY missing!")
+	end
+
+	-- get game area width
+	local n = PresetValue("GameAreaWidth")
+	
+	if n > 0 then
+		game_data.areas[GAME_AREA].width = tonumber(preset_data[n][2])
+	else
+		print("GameAreaWidth missing!")
+	end
+
+	-- get game area height
+	local n = PresetValue("GameAreaHeight")
+	
+	if n > 0 then
+		game_data.areas[GAME_AREA].height = tonumber(preset_data[n][2])
+	else
+		print("GameAreaHeight missing!")
+	end
+
+	-- get score area x
+	local n = PresetValue("ScoreAreaX")
+	
+	if n > 0 then
+		table.insert(game_data.areas, {x = tonumber(preset_data[n][2])})
+	else
+		print("ScoreAreaX missing!")
+	end
+
+	-- get score area y
+	local n = PresetValue("ScoreAreaY")
+	
+	if n > 0 then
+		game_data.areas[SCORE_AREA].y = tonumber(preset_data[n][2])
+	else
+		print("ScoreAreaY missing!")
+	end
+
+	-- get score area width
+	local n = PresetValue("ScoreAreaWidth")
+	
+	if n > 0 then
+		game_data.areas[SCORE_AREA].width = tonumber(preset_data[n][2])
+	else
+		print("ScoreAreaWidth missing!")
+	end
+
+	-- get score area height
+	local n = PresetValue("ScoreAreaHeight")
+	
+	if n > 0 then
+		game_data.areas[SCORE_AREA].height = tonumber(preset_data[n][2])
+	else
+		print("ScoreAreaHeight missing!")
+	end
+
+	-- get lives area x
+	local n = PresetValue("LivesAreaX")
+	
+	if n > 0 then
+		table.insert(game_data.areas, {x = tonumber(preset_data[n][2])})
+	else
+		print("LivesAreaX missing!")
+	end
+
+	-- get lives area y
+	local n = PresetValue("LivesAreaY")
+	
+	if n > 0 then
+		game_data.areas[LIVES_AREA].y = tonumber(preset_data[n][2])
+	else
+		print("LivesAreaY missing!")
+	end
+
+	-- get lives area width
+	local n = PresetValue("LivesAreaWidth")
+	
+	if n > 0 then
+		game_data.areas[LIVES_AREA].width = tonumber(preset_data[n][2])
+	else
+		print("LivesAreaWidth missing!")
+	end
+
+	-- get lives area height
+	local n = PresetValue("LivesAreaHeight")
+	
+	if n > 0 then
+		game_data.areas[LIVES_AREA].height = tonumber(preset_data[n][2])
+	else
+		print("LivesAreaHeight missing!")
+	end
+
+	-- get level area x
+	local n = PresetValue("LevelAreaX")
+	
+	if n > 0 then
+		table.insert(game_data.areas, {x = tonumber(preset_data[n][2])})
+	else
+		print("LevelAreaX missing!")
+	end
+
+	-- get level area y
+	local n = PresetValue("LevelAreaY")
+	
+	if n > 0 then
+		game_data.areas[LEVEL_AREA].y = tonumber(preset_data[n][2])
+	else
+		print("LevelAreaY missing!")
+	end
+
+	-- get level area width
+	local n = PresetValue("LevelAreaWidth")
+	
+	if n > 0 then
+		game_data.areas[LEVEL_AREA].width = tonumber(preset_data[n][2])
+	else
+		print("LevelAreaWidth missing!")
+	end
+
+	-- get level area height
+	local n = PresetValue("LevelAreaHeight")
+	
+	if n > 0 then
+		game_data.areas[LEVEL_AREA].height = tonumber(preset_data[n][2])
+	else
+		print("LevelAreaHeight missing!")
+	end
+
+	-- get fonts
+	local n = PresetValue("Fonts")
+	
+	if n > 0 then
+		game_data.fonts = preset_data[n][2]
+	else
+		print("Fonts missing!")
+	end
+	
+	ResetAll()
+end
+
+-- find preset value
+function PresetValue(v)
+	for i = 1, #preset_data do
+		if preset_data[i][1] == v then
+			return i
+		end
+	end
+	
+	return 0
+end
+
+-- change pen's ink
+function ChangePensInk(p, incdec)
+	if incdec > 0 and game_data.pens_palette[p + 1] < game_data.max_inks - 1 then
+		game_data.pens_palette[p + 1] = game_data.pens_palette[p + 1] + incdec
+	elseif incdec < 0 and game_data.pens_palette[p + 1] > 0 then
+		game_data.pens_palette[p + 1] = game_data.pens_palette[p + 1] + incdec
+	end
+end
+
+-- show a cursor to draw, or other stuffs
+function ShowCursor(x, y, w, h, r, g, b)
+	love.graphics.setColor(r, g, b)
+	love.graphics.rectangle("line", x + 1, y + 1, w - 2, h - 2)
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.rectangle("line", x, y, w, h)
+end
+
+-- return if they are too many colors
+function TooManyColors(t, p)
+	local col = {}
+	local cpt = 0
+	
+	for x = 0, game_data.block_width - 1 do
+		for y = 0, game_data.block_height - 1 do
+			local flag = false
+			
+			for i = 1, #col do
+				if col[i] == t[x][y] then
+					flag = true
+				end
+			end
+			
+			if flag == false then
+				table.insert(col, t[x][y])
+				cpt = cpt + 1
+			end
+		end
+	end
+	
+	if cpt <= game_data.colors_by_block then
+		return false
+	end
+	
+	return true
+end
+
+-- fill a block or a sprite
+function FloodFill(t, w, h, x, y, newColor, oldColor)
+	if x >= 0 and x < w and y >= 0 and y < h and t[x][y] == oldColor and t[x][y] ~= newColor then
+		t[x][y] = newColor
+		
+		FloodFill(t, w, h, x + 1, y, newColor, oldColor)
+		FloodFill(t, w, h, x - 1, y, newColor, oldColor)
+		FloodFill(t, w, h, x, y + 1, newColor, oldColor)
+		FloodFill(t, w, h, x, y - 1, newColor, oldColor)
+	end
+end
+
+-- count actor's types
+function CountAnimationsPairs()
+	local counter = 0
+	
+	for k, v in pairs(game_data.actors[current_actor].type) do
+		counter = counter + 1
+	end
+	
+	return counter
+end
+
+-- update level_data and delete all level's blocks and sprites!
+function UpdateLevelsData()
+	game_data.levels_data = {
+		w = 1,
+		h = 1,
+		sw = math.floor(game_data.areas[GAME_AREA].width / game_data.block_width),
+		sh = math.floor(game_data.areas[GAME_AREA].height / game_data.block_height)
+	}
+	
+	local cpt_levels = #game_data.levels
+	
+	game_data.levels = {}
+
+	-- prepare void level
+	local t = {
+		blocks = {},
+		actors = {}
+	}
+					
+	-- fill void level
+	for x = 0, (game_data.levels_data.sw * game_data.levels_data.w) - 1 do
+		t.blocks[x] = {}
+				
+		for y = 0, (game_data.levels_data.sh * game_data.levels_data.h) - 1 do
+			t.blocks[x][y] = 0
+		end
+	end
+	
+	-- add player actor to the level
+	table.insert(t.actors, {startx = 0, starty = 0, x = 0, y = 0, animation = 0, frame = 0})
+	
+	for i = 1, cpt_levels do
+		table.insert(game_data.levels, t)
+	end
+end
+
+
---
+++ RetroCloner/run.lua
@@ -0,0 +1,154 @@
+local run = {}
+
+
+-- run constants
+local RUN_MAX_ANIMATION_SPEED = 0.25
+
+-- run arrays
+run.vars = {
+	score = 0,
+	level = 0,
+	scrolling_x = 0,
+	scrolling_y = 0,
+	animation_timer = 0.0
+}
+
+-- run vars
+tick = false
+
+-- run functions
+function run.load()
+	run.vars.score = 0
+	run.vars.level = 1
+	
+	run.vars.scrolling_x = game_data.scrolling_start_x[run.vars.level]
+	run.vars.scrolling_y = game_data.scrolling_start_y[run.vars.level]
+	
+	run.vars.animation_timer = 0.0
+	
+	-- reset actors positions and animations
+	for i = 1, #game_data.levels do
+		for j = 1, #game_data.levels[i].actors do
+			game_data.levels[i].actors[j].x = game_data.levels[i].actors[j].start_x
+			game_data.levels[i].actors[j].y = game_data.levels[i].actors[j].start_y
+			game_data.levels[i].actors[j].animation = GetActorAnimationNumber(game_data.levels[i].actors[j].number, "idle")
+			game_data.levels[i].actors[j].frame = 1
+		end
+	end
+end
+
+function run.update(dt)
+	run.vars.animation_timer = run.vars.animation_timer + dt
+	tick = false
+	
+	if run.vars.animation_timer >= RUN_MAX_ANIMATION_SPEED then
+		run.vars.animation_timer = 0.0
+		tick = true
+	end
+	
+	if tick == true then
+		-- animate characters
+		if #game_data.levels > 0 then
+			for i = 1, #game_data.levels[run.vars.level].actors do
+				if game_data.levels[run.vars.level].actors[i].frame < #game_data.animations[game_data.levels[run.vars.level].actors[i].animation] then
+					game_data.levels[run.vars.level].actors[i].frame = game_data.levels[run.vars.level].actors[i].frame + 1
+				elseif game_data.levels[run.vars.level].actors[i].frame == #game_data.animations[game_data.levels[run.vars.level].actors[i].animation] then
+					game_data.levels[run.vars.level].actors[i].frame = 1
+				end
+			end
+		end
+	end
+end
+
+function run.draw()
+  	-- adapt resolution to window (or fullscreen)
+    love.graphics.scale(love.graphics.getWidth() / (WINDOW_WIDTH + (WINDOW_BORDER * 2)), love.graphics.getHeight() / (WINDOW_HEIGHT + (WINDOW_BORDER * 2)))
+
+	-- ===========================================================================================================================================================================
+
+	-- disable scissor
+	love.graphics.setScissor()
+
+	-- clear with border color
+	r, g, b = GetPenRGB(game_data.border_paper)
+	love.graphics.clear(r, g, b)
+
+	-- ===========================================================================================================================================================================
+	
+	-- set scissor without the border
+	love.graphics.setScissor(WINDOW_BORDER, WINDOW_BORDER, WINDOW_WIDTH, WINDOW_HEIGHT)
+
+	-- clear the background with background paper color
+	r, g, b = GetPenRGB(game_data.background_paper)
+	love.graphics.clear(r, g, b)
+
+	-- draw game area
+	r, g, b = GetPenRGB(game_data.game_paper)
+	love.graphics.setColor(r, g, b)
+	love.graphics.rectangle("fill", ScaleX(game_data.areas[GAME_AREA].x, WINDOW_ZOOM), ScaleY(game_data.areas[GAME_AREA].y, WINDOW_ZOOM), ScaleWidth(game_data.areas[GAME_AREA].width, WINDOW_ZOOM), ScaleHeight(game_data.areas[GAME_AREA].height, WINDOW_ZOOM))
+
+	-- ===========================================================================================================================================================================
+
+	-- set scissor to game area
+	love.graphics.setScissor(ScaleX(game_data.areas[GAME_AREA].x, WINDOW_ZOOM), ScaleY(game_data.areas[GAME_AREA].y, WINDOW_ZOOM), ScaleWidth(game_data.areas[GAME_AREA].width, WINDOW_ZOOM), ScaleHeight(game_data.areas[GAME_AREA].height, WINDOW_ZOOM))
+
+	-- draw the game
+	DrawGame()
+
+	-- ===========================================================================================================================================================================
+	
+	-- disable scissor
+	love.graphics.setScissor()
+
+	-- draw score area
+	FontsPrint("SCORE 0000000", ScaleX(game_data.areas[SCORE_AREA].x, WINDOW_ZOOM), ScaleY(game_data.areas[SCORE_AREA].y, WINDOW_ZOOM), game_data.areas[SCORE_AREA].width * WINDOW_ZOOM, game_data.areas[SCORE_AREA].height * WINDOW_ZOOM, GAME_FONT, FONT_DOWN_SCALE / WINDOW_ZOOM, game_data.text_paper, game_data.text_pen)
+
+	-- draw lives area
+	FontsPrint("LIVES 03", ScaleX(game_data.areas[LIVES_AREA].x, WINDOW_ZOOM), ScaleY(game_data.areas[LIVES_AREA].y, WINDOW_ZOOM), game_data.areas[LIVES_AREA].width * WINDOW_ZOOM, game_data.areas[LIVES_AREA].height * WINDOW_ZOOM, GAME_FONT, FONT_DOWN_SCALE / WINDOW_ZOOM, game_data.text_paper, game_data.text_pen)
+
+	-- draw level area
+	FontsPrint("LEVEL 1", ScaleX(game_data.areas[LEVEL_AREA].x, WINDOW_ZOOM), ScaleY(game_data.areas[LEVEL_AREA].y, WINDOW_ZOOM), game_data.areas[LEVEL_AREA].width * WINDOW_ZOOM, game_data.areas[LEVEL_AREA].height * WINDOW_ZOOM, GAME_FONT, FONT_DOWN_SCALE / WINDOW_ZOOM, game_data.text_paper, game_data.text_pen)
+end
+
+-- draw the game
+function DrawGame()
+	love.graphics.setColor(1, 1, 1)
+	
+	-- game area position
+	local px = ScaleX(game_data.areas[GAME_AREA].x, WINDOW_ZOOM)
+	local py = ScaleY(game_data.areas[GAME_AREA].y, WINDOW_ZOOM)
+	
+	local offset_x = math.floor((game_data.areas[GAME_AREA].width - (game_data.levels_data.sw * game_data.block_width)) / 2)
+	local offset_y = math.floor((game_data.areas[GAME_AREA].height - (game_data.levels_data.sh * game_data.block_height)) / 2)
+
+	px = px + ScaleWidth(offset_x, game_data.pixel_size, WINDOW_ZOOM)
+	py = py + ScaleHeight(offset_y, WINDOW_ZOOM)
+	
+	if #game_data.levels > 0 then
+		-- draw blocks
+		for x = 0, (game_data.levels_data.sw * game_data.levels_data.w) - 1 do
+			for y = 0, (game_data.levels_data.sh * game_data.levels_data.h) - 1 do
+				local block = game_data.levels[run.vars.level].blocks[x][y]
+				local xc = x * game_data.block_width * game_data.pixel_size * WINDOW_ZOOM
+				local yc = y * game_data.block_height * WINDOW_ZOOM
+				
+				if block > 0 then
+					love.graphics.draw(img_blocks[block], px + xc, py + yc, 0, game_data.pixel_size * WINDOW_ZOOM, WINDOW_ZOOM)
+				end
+			end
+		end
+	
+		-- draw actors
+		for i = 1, #game_data.levels[run.vars.level].actors do
+			local animation = game_data.levels[run.vars.level].actors[i].animation
+			local frame = game_data.levels[run.vars.level].actors[i].frame
+			
+			if animation > 0 and frame > 0 then
+				local sprite = game_data.animations[animation][frame]
+				love.graphics.draw(img_sprites[sprite], px + game_data.levels[run.vars.level].actors[i].x, py + game_data.levels[run.vars.level].actors[i].y, 0, game_data.pixel_size * WINDOW_ZOOM, WINDOW_ZOOM)
+			end
+		end
+	end
+end
+
+return run
